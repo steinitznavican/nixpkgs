15f749109e822309a2e981de876f400369a00c87
Florian Klink <flokli@flokli.de>
2018-04-30 22:26:40 +0200

    poppler: multi-output (WIP)

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -1,10 +1,7 @@
 { stdenv, lib, fetchurl, cmake, ninja, pkgconfig, libiconv, libintl
 , zlib, curl, cairo, freetype, fontconfig, lcms, libjpeg, openjpeg
+, gobjectIntrospection, qt5
 , withData ? true, poppler_data
-, qt5Support ? false, qtbase ? null
-, introspectionSupport ? false, gobjectIntrospection ? null
-, utils ? false
-, minimal ? false, suffix ? "glib"
 }:
 
 let # beware: updates often break cups-filters build
@@ -12,23 +9,23 @@ let # beware: updates often break cups-filters build
   mkFlag = optset: flag: "-DENABLE_${flag}=${if optset then "on" else "off"}";
 in
 stdenv.mkDerivation rec {
-  name = "poppler-${suffix}-${version}";
+  name = "poppler-${version}";
 
   src = fetchurl {
     url = "${meta.homepage}/poppler-${version}.tar.xz";
     sha256 = "04d1z1ygyb3llzc6s6c99wxafvljj2sc5b76djif34f7mzfqmk17";
   };
 
-  outputs = [ "out" "dev" ];
+  outputs = [ "out" "dev" "qt5" "glib" "utils" ];
 
   buildInputs = [ libiconv libintl ] ++ lib.optional withData poppler_data;
 
   # TODO: reduce propagation to necessary libs
   propagatedBuildInputs = with lib;
     [ zlib freetype fontconfig libjpeg openjpeg ]
-    ++ optionals (!minimal) [ cairo lcms curl ]
-    ++ optional qt5Support qtbase
-    ++ optional introspectionSupport gobjectIntrospection;
+    ++ [ cairo lcms curl ]
+    ++ [ qt5.qtbase ]
+    ++ [ gobjectIntrospection ];
 
   nativeBuildInputs = [ cmake ninja pkgconfig ];
 
@@ -37,12 +34,31 @@ stdenv.mkDerivation rec {
 
   cmakeFlags = [
     (mkFlag true "XPDF_HEADERS")
-    (mkFlag (!minimal) "GLIB")
-    (mkFlag (!minimal) "CPP")
-    (mkFlag (!minimal) "LIBCURL")
-    (mkFlag utils "UTILS")
-    (mkFlag qt5Support "QT5")
+    (mkFlag true "GLIB") # TODO output
+    (mkFlag true "CPP")
+    (mkFlag true "LIBCURL")
+    (mkFlag true "UTILS") #TODO output
+    (mkFlag true "QT5") # TODO output, qt4?
   ];
+
+  postInstall = ''
+    find $out
+
+    # glib
+    moveToOutput $out/include/poppler/glib $glib/include/poppler/
+    moveToOutput $out/lib/libpoppler-glib.* $glib/lib/
+    moveToOutput $out/lib/pkgconfig/poppler-glib.pc $glib/lib/pkgconfig/
+    moveToOutput $out/lib/girepository-* $glib/lib/
+    moveToOutput $out/share/gir-* $glib/share/
+
+    # utils
+    moveToOutput $out/bin $utils/
+
+    # qt5
+    moveToOutput $out/include/poppler/qt5 $qt5/include/poppler/
+    moveToOutput $out/lib/libpoppler-qt5.* $qt5/lib
+    moveToOutput $out/lib/pkgconfig/poppler-qt5.pc $qt5/lib/pkgconfig/
+  '';
 
   meta = with lib; {
     homepage = https://poppler.freedesktop.org/;
