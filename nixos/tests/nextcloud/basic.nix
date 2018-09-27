adad63aa824e137bc3bfea883f414ec7b8b19e26
Ruben Maher <ruben@maher.fyi>
2018-09-27 20:35:58 +0930

    nextcloud: also try access sqlite-backed test with webdav

--- nixos/tests/nextcloud/basic.nix
+++ nixos/tests/nextcloud/basic.nix
@@ -5,20 +5,57 @@ import ../make-test.nix ({ pkgs, ...} : {
   };
 
   nodes = {
+    client = { config, pkgs, ... }: {
+      services.davfs2.enable = true;
+
+      environment.etc."davfs2/secrets" = {
+        mode = "600";
+        text = ''
+          http://nextcloud/remote.php/dav/files/root root notproduction
+        '';
+      };
+    };
+
     nextcloud = { config, pkgs, ... }: {
+      networking.firewall.allowedTCPPorts = [ 80 ];
+
       services.nextcloud = {
         enable = true;
         nginx.enable = true;
         hostName = "nextcloud";
         config.adminpass = "notproduction";
       };
+
+      services.davfs2.enable = true;
+
+      environment.etc."davfs2/secrets" = {
+        mode = "600";
+        text = ''
+          http://nextcloud/remote.php/dav/files/root root notproduction
+        '';
+      };
     };
+
   };
 
-  testScript = ''
-    $nextcloud->start();
-    $nextcloud->waitForUnit("nginx");
-    $nextcloud->waitForUnit("phpfpm-nextcloud");
+  testScript = let
+    diffSharedFile = pkgs.writeScript "diff-shared-file" ''
+      #!${pkgs.stdenv.shell}
+      echo "waiting for FUSE..."
+      sleep 10
+
+      diff <(echo 'hi') /mnt/shared-file
+    '';
+  in ''
+    startAll();
+    $nextcloud->waitForUnit("multi-user.target");
     $nextcloud->succeed("curl -sSf http://nextcloud/login");
+    $nextcloud->succeed("mkdir /mnt && mount -t davfs http://nextcloud/remote.php/dav/files/root /mnt");
+    $nextcloud->waitForUnit("sys-fs-fuse-connections.mount");
+    $nextcloud->succeed("echo 'hi' > /mnt/shared-file");
+    $client->waitForUnit("multi-user.target");
+    $client->succeed("mkdir /mnt && mount -t davfs http://nextcloud/remote.php/dav/files/root /mnt");
+    $client->waitForUnit("sys-fs-fuse-connections.mount");
+    $client->succeed("${diffSharedFile}");
   '';
 })
