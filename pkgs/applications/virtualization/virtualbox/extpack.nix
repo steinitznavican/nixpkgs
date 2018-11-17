e51df87321606781818e58725abde088ca805f85
Florian Klink <flokli@flokli.de>
2018-11-17 17:44:10 +0100

    virtualboxExtpack: update to be a drv with required directory structure

--- pkgs/applications/virtualization/virtualbox/extpack.nix
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -1,16 +1,30 @@
-{stdenv, fetchurl, lib}:
+{ stdenv, fetchurl, lib }:
 
-with lib;
-
-let extpackRev = "123301";
-    version = "5.2.14";
+let name = "Oracle_VM_VirtualBox_Extension_Pack";
+  version = "5.2.14";
+  extpackRev = "123301";
 in
-fetchurl rec {
-  name = "Oracle_VM_VirtualBox_Extension_Pack-${version}-${toString extpackRev}.vbox-extpack";
-  url = "http://download.virtualbox.org/virtualbox/${version}/${name}";
-  sha256 = "d90c1b0c89de19010f7c7fe7a675ac744067baf29a9966b034e97b5b2053b37e";
 
-  meta = {
+stdenv.mkDerivation {
+  inherit name version;
+
+  src = fetchurl {
+    url = "http://download.virtualbox.org/virtualbox/${version}/${name}-${version}-${toString extpackRev}.vbox-extpack";
+    sha256 = "d90c1b0c89de19010f7c7fe7a675ac744067baf29a9966b034e97b5b2053b37e";
+  };
+
+  unpackCmd = ''
+    mkdir -p out
+    tar xf $curSrc -C out
+  '';
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/${name}
+    cp -R * $out/${name}/
+  '';
+
+  meta = with stdenv.lib; {
     description = "Oracle Extension pack for VirtualBox";
     license = licenses.virtualbox-puel;
     homepage = https://www.virtualbox.org/;
